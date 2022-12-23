45002db7758732d692ea6b64e90c238888e326fa
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-12-23 11:00:43 +0700

    done

---
+++ src/main/java/com/htv3/htv3onlinemusic/Htv3OnlineMusicApplication.java
@@ -0,0 +1,12 @@
+package com.htv3.htv3onlinemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Htv3OnlineMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Htv3OnlineMusicApplication.class, args);
+    }
+}
---
+++ src/main/java/com/htv3/htv3onlinemusic/service/role/IRoleService.java
@@ -0,0 +1,7 @@
+package com.htv3.htv3onlinemusic.service.role;
+
+import com.htv3.htv3onlinemusic.model.entity.Role;
+import com.htv3.htv3onlinemusic.service.GenericService;
+
+public interface IRoleService extends GenericService<Role> {
+}
